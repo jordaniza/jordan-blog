12468891645740d57a0bf674a6bc76e3d0e7fa96
jordan <jordaniza@proton.me>
2023-05-12 11:22:58 +0400

    article 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "github-dark",
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-
-	},
-	plugins: [
-    require('@tailwindcss/typography')
-  ],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      overflow: ["break-word"],
+      typography: {
+        DEFAULT: {
+          css: {
+            pre: {
+              "overflow-wrap": "anywhere",
+              wordWrap: "break-word",
+              "overflow-x": "auto",
+              "white-space": "pre",
+            },
+            "code::before": { content: "" },
+            "code::after": { content: "" },
+          },
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
