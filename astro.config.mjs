0608d471397e089619f5d74144ddda28c340f337
jordan <jordaniza@proton.me>
2023-05-13 13:10:55 +0400

    solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), solidJs()],
   markdown: {
     shikiConfig: {
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       theme: "github-dark",
       // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-    },
-  },
+      wrap: true
+    }
+  }
 });
